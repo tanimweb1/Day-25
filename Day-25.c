678b621d22121aaaeef3b2d2d5d80ade67f8897b
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-11 20:46:40 +0600

    Update Day-25.c

--- Day-25.c
+++ Day-25.c
@@ -17,6 +17,30 @@ else {
 
 
 
+// bee 1036
+
+double a,b,c,rat,x1,x2;
+scanf("%lf%lf%lf", &a,&b,&c);
+
+
+rat = (b*b-4*a*c);
+
+
+if(rat>0 && a!=0){
+   x1= (-b+sqrt(rat))/(2*a);
+    x2= (-b-sqrt(rat))/(2*a);
+  
+printf("R1 = %0.5lf\n",x1);
+printf("R2 = %0.5lf\n",x2);
+}
+
+else{
+    printf("Impossivel calcular\n");
+}
+
+    
+
     return 0;
 }
+
 
